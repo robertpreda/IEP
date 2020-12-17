e8be407841414c85fb4bb42358c27f2b1eb099c7
Robert Preda <predarobert7@gmail.com>
2020-12-17 17:25:15 +0200

    Added menu in main function

---
+++ lab8/build/CMakeFiles/figures.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/figures.dir/main.cpp.o"
+  "figures.pdb"
+  "figures"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/figures.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- lab8/main.cpp
+++ lab8/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <iterator>
+#include <list>
 #include <cmath>
 
 using namespace std;
@@ -54,6 +55,18 @@ class Figure{
         vector<Point> getPoints(){
             return points;
         }
+
+        void modifyPoint(int index, Point newPoint){
+            points[index] = newPoint;
+        }
+
+        void addPoint(Point p){
+            points.push_back(p);
+        }
+
+        virtual string classname(){
+
+        }
 };
 
 class Circle:public Figure{
@@ -86,6 +99,27 @@ class Circle:public Figure{
             }
         }
 
+        static Circle *getInstance(){
+            return instance;
+        }
+
+        void modifyOrigin(Point newPoint){
+            origin = newPoint;
+        }
+
+        void addPoint(Point p){
+            cout << "For circle, use the modifyPoint method\n";
+        }
+
+        string classname(){
+            return "circle";
+        }
+
+        Point getOrigin(){
+            return instance->origin;
+        }
+
+
         
         
 };
@@ -93,11 +127,12 @@ class Circle:public Figure{
 
 class Rectangle:public Figure{
     public:
-        Rectangle(Point a, Point b, Point c, Point d){
-            points.push_back(a);
-            points.push_back(b);
-            points.push_back(c);
-            points.push_back(d);
+        Rectangle(){
+            cout << "Created new rectangle \n";
+        }
+
+        string classname(){
+            return "rectangle";
         }
 
         
@@ -105,16 +140,27 @@ class Rectangle:public Figure{
 
 class Triangle:public Figure{
     public:
-        Triangle(Point a, Point b, Point c){
-            points.push_back(a);
-            points.push_back(b);
-            points.push_back(c);
+        Triangle(){
+            cout << "Created new triangle \n";
+        }
+        string classname(){
+            return "triangle";
+        }
+};
+
+class Pentagon:public Figure{
+    public:
+        Pentagon(){
+           cout << "Ceated new pentagon \n";
+        }
+        string classname(){
+            return "pentagon";
         }
 };
 
 template <class Figure>
 float getSlopeOf2Points(Figure f){
-    vector<Point> points = f.getPoints();
+    vector<Point> points = f->getPoints();
     Point point_a = points[0];
     Point point_b = points[1];
 
@@ -128,10 +174,12 @@ float getSlopeOf2Points(Figure f){
 }
 
 template <class Figure>
-bool figureOverlapsCircle(Figure f, Circle c){
-    vector<Point> figPoint = f.getPoints();
-    Point center = c.origin;
-    int radius = c.radius;
+bool figureOverlapsCircle(Figure *f){
+    vector<Point> figPoint = f->getPoints();
+    
+    Circle *circ = Circle::getInstance(); 
+    Point center = circ->getOrigin();
+    int radius = circ->getRadius();
     for(int i = 0; i < figPoint.size(); i++){
         int x = figPoint[i].getX();
         int y = figPoint[i].getY();
@@ -139,13 +187,120 @@ bool figureOverlapsCircle(Figure f, Circle c){
         int a = center.getX();
         int b = center.getY();
 
-        if(pow(x-a, 2) + pow(y-b,2) = pow(radius,2))
+        if(pow(x-a, 2) + pow(y-b,2) == pow(radius,2))
             return true;
     }
     return false;
 }
 
+Circle *Circle::instance = 0;
+
 
 int main(void){
+    int choice;
+    vector<Figure*> shapes; 
+    cout << "Enter a choice: \n";
+    cout << "1 - New rectangle \n";
+    cout << "2 - New triangle \n";
+    cout << "3 - New cicle \n"; 
+    cout << "4 - New pentagon \n";
+    cout << "5 - Slope of side of shape: \n";
+    cout << "6 - Check for overlap with circle: \n";
+    cout << "7 - Exit\n";
+    while(true){
+    cin >> choice; cout << "\n";            
+    switch(choice){
+        case 1:{
+            Rectangle *r = new Rectangle();
+            for(int i = 0; i<4;i++){
+                cout << "Enter coordonates for point " << i << " :\n";
+                int x, y;
+                cout << "x = "; cin >> x; cout << " \n";
+                cout << "y = "; cin >> y; cout << " \n";
+                Point p(x, y);
+                r->addPoint(p);
+            }
+                shapes.push_back(r);
+                break;
+            }
+        case 2:
+            {
+                Triangle *t = new Triangle();
+                for(int i = 0; i<3;i++){
+                    cout << "Enter coordonates for point " << i << " :\n";
+                    int x, y;
+                    cout << "x = "; cin >> x; cout << " \n";
+                    cout << "y = "; cin >> y; cout << " \n";
+                    Point p(x, y);
+                    t->addPoint(p);
+                }
+                shapes.push_back(t);
+                break;
+            }
+        case 3:
+            
+           { 
+                cout << "Enter origin coords: \n";
+                int x,y,rad;
+                cout << "x = "; cin >> x; cout << "\n";
+                cout << "y = "; cin >> y; cout << "\n";
+                cout << "Enter radius: "; cin >> rad; cout << "\n";
+                Point origin(x,y);
+                Circle *c = Circle::getCircle(origin, rad);
+                break;
+            }
+        case 4:
+            {
+                Pentagon *pent = new Pentagon();
+                for(int i = 0; i<5;i++){
+                    cout << "Enter coordonates for point " << i << " :\n";
+                    int x, y;
+                    cout << "x = "; cin >> x; cout << " \n";
+                    cout << "y = "; cin >> y; cout << " \n";
+                    Point p(x, y);
+                    pent->addPoint(p);
+                }
+                shapes.push_back(pent);
+                break;
+            }
+        
+        case 5:
+            {
+                int index;
+                cout << "Choose between 1 and " << shapes.size() << "\n";
+                cin >> index;
+                Figure *f = shapes[index];
+               
+                float m = getSlopeOf2Points(f);
+                cout << "Slope: " << m << "\n";
+
+                break;
+            }
+
+        case 6:
+            {
+                int index;
+                cout << "Choose between 0 and " << shapes.size()-1 << "\n";
+                cin >> index;
+                Figure *f = shapes[index];
+                Circle *c = Circle::getInstance();
+                if(!c){
+                    cout << "Circle instance has not been initialized\n";
+                }else{
+                    cout << "Chosen shape overlaps with circle: " << figureOverlapsCircle(f) << "\n";
+                }
+            break;
+
+            }
+
+        case 7:{
+            cout << "Bye\n";
+            return 0;
+            }
+
+        default:
+            cout << "Bad choice, try again \n";
+        }
+    }
     return 0;
 }
